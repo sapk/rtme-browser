b8ac21931a719b63de4062bb226358314deccc89
Antoine GIRARD <sapk@users.noreply.github.com>
2020-05-05 10:18:38 +0200

    lint: go dot

--- module/config/config.go
+++ module/config/config.go
@@ -6,7 +6,7 @@ import (
 	"github.com/sapk/go-genesys/db"
 )
 
-//Config hold the config and db access
+//Config hold the config and db access.
 type Config struct {
 	DB *db.DB
 }
@@ -17,12 +17,12 @@ var (
 
 //TODO manage lock
 
-//IsNotInit state if the config is not init
+//IsNotInit state if the config is not init.
 func IsNotInit() bool {
 	return config == nil
 }
 
-//Setup start the db connexion
+//Setup start the db connexion.
 func Setup(dbType, dbRTMEURL, dbCFGURL string) error {
 	d := db.NewDBFromURL(dbType, dbCFGURL, dbRTMEURL)
 	e, err := d.CFG()
@@ -47,7 +47,7 @@ func Setup(dbType, dbRTMEURL, dbCFGURL string) error {
 	return nil
 }
 
-//Get give access to the config object
+//Get give access to the config object.
 func Get() (*Config, error) {
 	if IsNotInit() {
 		return nil, fmt.Errorf("config is not set")
